810fa302d07d4f20df4c3a82728a428a4bf1534b
kjtbw <pxp7707l@s.okayama-u.ac.jp>
2017-03-14 13:57:03 +0900

    get player number

--- include/mandamu.h
+++ include/mandamu.h
@@ -1,2 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define PNII -1
+
+int PLAYER_NUMBER;
+
+void initialize();
+int print_error(int n);
--- source/mandamu.c
+++ source/mandamu.c
@@ -1,12 +1,39 @@
 #include "mandamu.h"
 
 
+
 int main(void){
+	char buf[256];
+	int checker=0;
+	
+	initialize();
+	printf("Let's start dungeon of mandamu!\n");
+	do{
+ 		printf("how many players are there?(2,3,4) \n");
+ 		fgets(buf, sizeof(buf)/sizeof(char),stdin);
+ 		if(atoi(buf) < 2 || 4 < atoi(buf))checker = print_error(PNII);
+ 		else checker =1;
+	}while(checker < 1);
+	PLAYER_NUMBER=atoi(buf);
+}
+
+void initialize(){
+	PLAYER_NUMBER=0;
+
+}
 
 
-system("clear");
-printf("ad");
 
+int print_error(int n){
+	switch(n){
+	case PNII:
+		printf("player numer is invalid. please input again.\n");
+		break;
+			
+	default: ;
+	
+	
+	}
 
-
+	return -1;
 }
